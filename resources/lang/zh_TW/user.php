8e8befa4c19b8a2995a44f2905cc168997ebabf1
yun <yun@project-studio.cc>
2024-07-12 15:50:38 +0000

    admin to add user

---
+++ resources/lang/zh_TW/user.php
@@ -0,0 +1,16 @@
+<?php 
+return [
+    'labels' => [
+        'User' => 'User',
+        'user' => 'User',
+    ],
+    'fields' => [
+        'name' => '使用者名稱',
+        'email' => '電子信箱',
+        'email_verified_at' => '驗證時間',
+        'password' => '密碼',
+        'remember_token' => 'remember_token',
+    ],
+    'options' => [
+    ],
+];
